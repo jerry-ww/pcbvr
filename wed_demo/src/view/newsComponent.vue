d90f70af89ee6f67d653692b47ee93e5218d7475
plant310 <2289791243@qq.com>
2019-08-14 09:42:37 +0800

    分页条居中

--- wed_demo/src/view/newsComponent.vue
+++ wed_demo/src/view/newsComponent.vue
@@ -1,71 +1,100 @@
 <template>
-    <div class="newsComponent">
+  <div class="newsComponent">
     <div class="newsye">
-        <div class="container">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <ul>
+              <li>
+                <a href style="color: #1e5077;font-weight: bold;">法标资讯</a>
+              </li>
+              <li>
+                <a href="news_2.html">行业资讯</a>
+              </li>
+              <li>
+                <a href>产品资讯</a>
+              </li>
+              <li>
+                <a href>媒体来风</a>
+              </li>
+              <li>
+                <a href>营销活动</a>
+              </li>
+              <li>
+                <a href>招商动态</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-9 news-col9">
             <div class="row">
-                <div class="col-md-3">
-                    <ul>
-                        <li><a href="" style="color: #1e5077;font-weight: bold;">法标资讯</a></li>
-                        <li><a href="news_2.html">行业资讯</a></li>
-                        <li><a href="">产品资讯</a></li>
-                        <li><a href="">媒体来风</a></li>
-                        <li><a href="">营销活动</a></li>
-                        <li><a href="">招商动态</a></li>
-
-                    </ul>
-                </div>
-                <div class="col-md-9 news-col9">
-                    <div class="row">
-                        <div class="col-md-4 col-sm-6" v-for="item in news" >
-                            <a href="">
-                                <img :src="item.img_url" alt="">
-                            </a>
-                            <span>{{item.date}}</span>
-                            <p>{{item.title}}</p>
-                            <p><a href="">了解详情</a></p>
-                        </div>
-                    </div>
-                    <nav aria-label="Page navigation">
-                        <ul class="pagination pagination-lg">
-                            <li  class="disabled">
-                                <a href="#" aria-label="Previous">
-                                    <span aria-hidden="true">&laquo;</span>
-                                </a>
-                            </li>
-                            <li><a href="news.html" style="background:#ccc;">1</a></li>
-                            <li><a href="">2</a></li>
-                            <li><a href="#">3</a></li>
-                            <li><a href="#">4</a></li>
-                            <li><a href="#">5</a></li>
-                            <li><a href="#">6</a></li>
-                            <li class="dinone"><a href="#">7</a></li>
-                            <li class="dinone"><a href="#">8</a></li>
-                            <li class="dinone"><a href="#">9</a></li>
-                            <li class="dinone"><a href="#">10</a></li>
-                            <li><a href="#">...</a></li>
-                            <li>
-                                <a href="#" aria-label="Next">
-                                    <span aria-hidden="true">&raquo;</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
+              <div class="col-md-4 col-sm-6" v-for="item in news">
+                <a href>
+                  <img :src="item.img_url" alt />
+                </a>
+                <span>{{item.date}}</span>
+                <p>{{item.title}}</p>
+                <p>
+                  <a href>了解详情</a>
+                </p>
+              </div>
             </div>
-
+            <nav class="nav" aria-label="Page navigation">
+              <ul class="pagination pagination-lg">
+                <li class="disabled">
+                  <a href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="news.html" style="background:#ccc;">1</a>
+                </li>
+                <li>
+                  <a href>2</a>
+                </li>
+                <li>
+                  <a href="#">3</a>
+                </li>
+                <li>
+                  <a href="#">4</a>
+                </li>
+                <li>
+                  <a href="#">5</a>
+                </li>
+                <li>
+                  <a href="#">...</a>
+                </li>
+                <li>
+                  <a href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
+      </div>
     </div>
     <!--回到顶部-->
     <!--在线客服-->
     <!--qq聊天代码部分begin-->
     <div class="izl-rmenu">
-        <a class="consult" target="_blank"><div class="phone" style="display:none;">0592-1234567</div></a>
-        <a class="cart"><div class="pic"></div></a>
-        <a href="javascript:void(0)" class="btn_top" style="display: block;"></a>
+      <a class="consult" target="_blank">
+        <div class="phone" style="display:none;">0592-1234567</div>
+      </a>
+      <a class="cart">
+        <div class="pic"></div>
+      </a>
+      <a href="javascript:void(0)" class="btn_top" style="display: block;"></a>
     </div>
-    <a target="_blank"  href="http://wpa.qq.com/msgrd?v=3&uin=123456&site=qq&menu=yes" id="udesk-feedback-tab" class="udesk-feedback-tab-left" style="display: block; background-color: black;"></a>
+    <a
+      target="_blank"
+      href="http://wpa.qq.com/msgrd?v=3&uin=123456&site=qq&menu=yes"
+      id="udesk-feedback-tab"
+      class="udesk-feedback-tab-left"
+      style="display: block; background-color: black;"
+    ></a>
     <!--qq聊天代码部分end-->
-    </div> 
+  </div>
 </template>
 <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
 <script src="../js/jquery-1.11.1.js"></script>
@@ -75,44 +104,49 @@
 <!--自己写的js-->
 <script src="../js/main.min.js"></script>
 <script>
-    export default {
-        data () {
-            return {
-                news:[
-                    {
-                        url:"",
-                        img_url:"http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
-                        date:"[ 2017-06-11 ]",
-                        title:"臻赢2018——马克森商学院营销型设计师特训营完美收官！"
-                    },
-                     {
-                        url:"",
-                        img_url:"http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
-                        date:"[ 2017-06-11 ]",
-                        title:"臻赢2018——马克森商学院营销型设计师特训营完美收官！"
-                    },
-                     {
-                        url:"",
-                        img_url:"http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
-                        date:"[ 2017-06-11 ]",
-                        title:"臻赢2018——马克森商学院营销型设计师特训营完美收官！"
-                    },
-                     {
-                        url:"",
-                        img_url:"http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
-                        date:"[ 2017-06-11 ]",
-                        title:"臻赢2018——马克森商学院营销型设计师特训营完美收官！"
-                    },
-                     {
-                        url:"",
-                        img_url:"http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
-                        date:"[ 2017-06-11 ]",
-                        title:"臻赢2018——马克森商学院营销型设计师特训营完美收官！"
-                    }
-                ]
-            }
+export default {
+  data() {
+    return {
+      news: [
+        {
+          url: "",
+          img_url:
+            "http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
+          date: "[ 2017-06-11 ]",
+          title: "臻赢2018——马克森商学院营销型设计师特训营完美收官！"
+        },
+        {
+          url: "",
+          img_url:
+            "http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
+          date: "[ 2017-06-11 ]",
+          title: "臻赢2018——马克森商学院营销型设计师特训营完美收官！"
+        },
+        {
+          url: "",
+          img_url:
+            "http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
+          date: "[ 2017-06-11 ]",
+          title: "臻赢2018——马克森商学院营销型设计师特训营完美收官！"
+        },
+        {
+          url: "",
+          img_url:
+            "http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
+          date: "[ 2017-06-11 ]",
+          title: "臻赢2018——马克森商学院营销型设计师特训营完美收官！"
+        },
+        {
+          url: "",
+          img_url:
+            "http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
+          date: "[ 2017-06-11 ]",
+          title: "臻赢2018——马克森商学院营销型设计师特训营完美收官！"
         }
-    }
+      ]
+    };
+  }
+};
 </script>
 <style  scoped>
 @import "../css/main.css";
--- wed_demo/src/view/showcaseComponent.vue
+++ wed_demo/src/view/showcaseComponent.vue
@@ -1,47 +1,111 @@
 <template>
   <div class="showcaseComponent" id="showcase">
-      <div class="products-yemian">
+    <div class="products-yemian">
       <div class="container">
-          <div class="row">
-              <div class="col-md-3 col-sm-4 products-leul">
-                  <h3>云景产品列表</h3>
-                  <ul class="tarr">
-                      <li class="products-leli"><a class="Js-prod-a1">全部产品</a></li>
-                      <li><a>校园</a></li>
-                      <li><a>景区</a></li>
-                      <li><a>商场</a></li>
-                      <li><a>火车站</a></li>
-                  </ul>
-                  <ul>
-                      <li class="products-leli"><a href="#" class="Js-prod-a1">最新发布</a></li>
-                      <li><a href="">武汉理工</a></li>
-                      <li><a href="">华中科技</a></li>
-                      <li><a href="">华中师范</a></li>
-                      <li><a href="">中南财大</a></li>
-                  </ul>
-                  <ul>
-                      <li class="products-leli"><a href="#" class="Js-prod-a1">最热搜索</a></li>
-                      <li><a href="">黄鹤楼</a></li>
-                      <li><a href="">晴川阁</a></li>
-                      <li><a href="">长江大桥</a></li>
-                  </ul>
-
-              </div>
-              <div class="col-md-9">
-                  <div class="row" >
-                      <div class="col-md-3 col-sm-3" v-for="item in products">
-                          <a :href="item.url"  class="products-border"><img :src="item.img_src" alt=""></a>
-                          <strong>{{item.classfication}}</strong>
-                          <p>{{item.name}}</p>
-                          <p>
-                          <a :href="item.detail_url" class="products-aniu">了解更多 <i class="fa fa-angle-right"></i></a>
-                          </p>
-                      </div>
-                  </div>
-              </div>
+        <div class="row">
+          <div class="col-md-3 col-sm-4 products-leul">
+            <h3>云景产品列表</h3>
+            <ul class="tarr">
+              <li class="products-leli">
+                <a class="Js-prod-a1">全部产品</a>
+              </li>
+              <li>
+                <a>校园</a>
+              </li>
+              <li>
+                <a>景区</a>
+              </li>
+              <li>
+                <a>商场</a>
+              </li>
+              <li>
+                <a>火车站</a>
+              </li>
+            </ul>
+            <ul>
+              <li class="products-leli">
+                <a href="#" class="Js-prod-a1">最新发布</a>
+              </li>
+              <li>
+                <a href>武汉理工</a>
+              </li>
+              <li>
+                <a href>华中科技</a>
+              </li>
+              <li>
+                <a href>华中师范</a>
+              </li>
+              <li>
+                <a href>中南财大</a>
+              </li>
+            </ul>
+            <ul>
+              <li class="products-leli">
+                <a href="#" class="Js-prod-a1">最热搜索</a>
+              </li>
+              <li>
+                <a href>黄鹤楼</a>
+              </li>
+              <li>
+                <a href>晴川阁</a>
+              </li>
+              <li>
+                <a href>长江大桥</a>
+              </li>
+            </ul>
           </div>
+          <div class="col-md-9">
+            <div class="row">
+              <div class="col-md-3 col-sm-3" v-for="item in products">
+                <a :href="item.url" class="products-border">
+                  <img :src="item.img_src" alt />
+                </a>
+                <strong>{{item.classfication}}</strong>
+                <p>{{item.name}}</p>
+                <p>
+                  <a :href="item.detail_url" class="products-aniu">
+                    了解更多
+                    <i class="fa fa-angle-right"></i>
+                  </a>
+                </p>
+              </div>
+            </div>
+            <nav class="nav" aria-label="Page navigation">
+              <ul class="pagination pagination-lg">
+                <li class="disabled">
+                  <a href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="news.html" style="background:#ccc;">1</a>
+                </li>
+                <li>
+                  <a href>2</a>
+                </li>
+                <li>
+                  <a href="#">3</a>
+                </li>
+                <li>
+                  <a href="#">4</a>
+                </li>
+                <li>
+                  <a href="#">5</a>
+                </li>
+                <li>
+                  <a href="#">...</a>
+                </li>
+                <li>
+                  <a href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
-  </div>
+    </div>
   </div>
 </template>
 <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
@@ -52,49 +116,59 @@
 <!--自己写的js-->
 <script src="../js/main.min.js"></script>
 <script>
-    export default {
-        data () {
-            return {
-                products:[
-                  {
-                  url:"https://www.baidu.com",
-                  img_src:"http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
-                  classfication:"图书馆",
-                  name:"校园",
-                  detail_url:"https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-2634161568.20.jpN6qf&id=523353236389"
-                  },
-                   {
-                  url:"https://www.baidu.com",
-                  img_src:"http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
-                  classfication:"图书馆",
-                  name:"校园",
-                  detail_url:"https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-2634161568.20.jpN6qf&id=523353236389"
-                  },
-                   {
-                  url:"https://www.baidu.com",
-                  img_src:"http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
-                  classfication:"图书馆",
-                  name:"校园",
-                  detail_url:"https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-2634161568.20.jpN6qf&id=523353236389"
-                  },
-                   {
-                  url:"https://www.baidu.com",
-                  img_src:"http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
-                  classfication:"图书馆",
-                  name:"校园",
-                  detail_url:"https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-2634161568.20.jpN6qf&id=523353236389"
-                  },
-                  {
-                  url:"https://www.baidu.com",
-                  img_src:"http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
-                  classfication:"图书馆",
-                  name:"校园",
-                  detail_url:"https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-2634161568.20.jpN6qf&id=523353236389"
-                  },
-              ]
-            }
+export default {
+  data() {
+    return {
+      products: [
+        {
+          url: "https://www.baidu.com",
+          img_src:
+            "http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
+          classfication: "图书馆",
+          name: "校园",
+          detail_url:
+            "https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-2634161568.20.jpN6qf&id=523353236389"
+        },
+        {
+          url: "https://www.baidu.com",
+          img_src:
+            "http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
+          classfication: "图书馆",
+          name: "校园",
+          detail_url:
+            "https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-2634161568.20.jpN6qf&id=523353236389"
+        },
+        {
+          url: "https://www.baidu.com",
+          img_src:
+            "http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
+          classfication: "图书馆",
+          name: "校园",
+          detail_url:
+            "https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-2634161568.20.jpN6qf&id=523353236389"
+        },
+        {
+          url: "https://www.baidu.com",
+          img_src:
+            "http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
+          classfication: "图书馆",
+          name: "校园",
+          detail_url:
+            "https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-2634161568.20.jpN6qf&id=523353236389"
+        },
+        {
+          url: "https://www.baidu.com",
+          img_src:
+            "http://101.132.40.201:8080/Myhouse1/picture/%E5%9B%BE%E4%B9%A6%E9%A6%86%E4%BE%A7%E9%9D%A2.jpg",
+          classfication: "图书馆",
+          name: "校园",
+          detail_url:
+            "https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-2634161568.20.jpN6qf&id=523353236389"
         }
-    }
+      ]
+    };
+  }
+};
 </script>
 <style  scoped>
 @import "../css/main.css";
